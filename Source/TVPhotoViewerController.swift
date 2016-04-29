8b2e15a1f7960e135dc114f471db1b60a2a4f0a8
Tai Vuong <vhuutai@gmail.com>
2016-04-29 15:47:28 +0700

    Added close button and implement action feature

--- Source/TVPhotoViewerController.swift
+++ Source/TVPhotoViewerController.swift
@@ -18,6 +18,10 @@ public protocol TVPhotoViewerDataSource : class {
 public protocol TVPhotoViewerDelegate : class {
     func photoViewerDidPresentPhotoAtIndex(photoViewer : TVPhotoViewerController , presentingPhotoIndex : Int)
     func photoViewerImageViewForPhotoIndex(photoViewer : TVPhotoViewerController , index : Int) -> UIImageView?
+    func photoViewerShouldPresentBuiltInActionSheetForPhotoAtIndex(photoViewer :TVPhotoViewerController , index : Int) -> Bool
+    func photoViewerShouldPresentCustomSheetForPhotoAtIndex(photoViewer : TVPhotoViewerController , index : Int)
+    func photoViewerActionsForPhotoAtIndex(photoViewer : TVPhotoViewerController , index : Int) -> [String]?
+    func photoViewerDidSelectActionForPhotoAtIndex(photoViewer : TVPhotoViewerController , action : String , index : Int)
 }
 
 public class TVPhotoViewerController : UIViewController {
@@ -30,7 +34,11 @@ public class TVPhotoViewerController : UIViewController {
     var pageScrollView : UIScrollView!
     var pagesContainer : UIPageViewController!
     var photoPagesArray : [TVPhotoPage]!
+    var closeButton : UIButton!
+    var actionButton : UIButton!
+    
     var viewModel : TVPhotoViewerViewModel!
+    var actions : [String]?
     
     var currentIndex = 0
     var totalImageCount = 0
@@ -54,7 +62,7 @@ public class TVPhotoViewerController : UIViewController {
     public override func loadView() {
         setupView()
         setupOverlayView()
-        setupTestGesture()
+        setupButtons()
         setupPhotoPageViewController()
     }
     
@@ -93,7 +101,7 @@ extension TVPhotoViewerController {
         pagesContainer.delegate = self
         pageScrollView = pagesContainer.pageScrollView
         
-        view.addSubview(pagesContainer.view)
+        view.insertSubview(pagesContainer.view, aboveSubview: overlayView)
         addChildViewController(pagesContainer)
         
         loadInitPhotoPage()
@@ -103,22 +111,9 @@ extension TVPhotoViewerController {
         if let firstPhotoPage = photoPageAtIndex(currentIndex) {
             firstPhotoPage.isInitialPhoto = true
             firstPhotoPage.initialImage = initialImage
+            updateCurrentImageIndex(currentIndex)
             pagesContainer.setViewControllers([firstPhotoPage], direction: .Forward, animated: false, completion: nil)
         }
-    }
-}
-
-//MARK: Gesture
-extension TVPhotoViewerController {
-    
-    func setupTestGesture() {
-        let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(doubleTap(_:)))
-        doubleTapGesture.numberOfTapsRequired = 2
-        view.addGestureRecognizer(doubleTapGesture)
-    }
-    
-    func doubleTap(gesture : UITapGestureRecognizer) {
-        dismissAnimated(true)
     }
 }
 
@@ -146,6 +141,7 @@ extension TVPhotoViewerController {
         
         UIView.animateWithDuration(animated ? animationDuration : 0, animations: { [unowned self] _ in
             self.overlayView.alpha = 0
+            self.hidingButtons()
             currentPhotoPage.restoreOriginFrame(dismissRect)
             }, completion: { _ in
                 self.dismissViewControllerAnimated(true, completion: { _ in
@@ -207,10 +203,96 @@ extension TVPhotoViewerController : UIPageViewControllerDataSource , UIPageViewC
         }
         
         if let photoPage = viewControllers.first as? TVPhotoPage {
-            currentIndex = photoPage.imageIndex
-            delegate?.photoViewerDidPresentPhotoAtIndex(self, presentingPhotoIndex: photoPage.photo.index)
-            currentImageView = delegate?.photoViewerImageViewForPhotoIndex(self, index: photoPage.imageIndex)
+            updateCurrentImageIndex(photoPage.imageIndex)
             currentImageView?.hidden = true
+        }
+    }
+    
+    func updateCurrentImageIndex(newIndex : Int) {
+        currentIndex = newIndex
+        delegate?.photoViewerDidPresentPhotoAtIndex(self, presentingPhotoIndex: newIndex)
+        currentImageView = delegate?.photoViewerImageViewForPhotoIndex(self, index: newIndex)
+        actions = delegate?.photoViewerActionsForPhotoAtIndex(self, index: newIndex)
+        actionButton?.hidden = (actions == nil)
+    }
+}
+
+//MARK: Action
+extension TVPhotoViewerController {
+    func setupButtons() {
+        setupActionButton()
+        setupCloseButton()
+        
+    }
+    
+    func hidingButtons() {
+        closeButton.alpha = 0
+        actionButton.alpha = 0
+    }
+    
+    func setupCloseButton() {
+        closeButton = UIButton(type: .Custom)
+        closeButton.frame = CGRectMake(0, 0, 50, 50)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setImage(UIImage(named: "ic_close"), forState: UIControlState.Normal)
+        closeButton.addTarget(self, action: #selector(closeButtonTapped), forControlEvents: .TouchUpInside)
+        
+        //Set up autolayout
+        let topConstraint = NSLayoutConstraint(item: view, attribute: .Top, relatedBy: .Equal, toItem: closeButton, attribute: .TopMargin, multiplier: 1, constant: -5)
+        let leadingConstraint = NSLayoutConstraint(item: view, attribute: .Leading, relatedBy: .Equal, toItem: closeButton, attribute: .LeadingMargin, multiplier: 1, constant: -5)
+        let widthConstraint = NSLayoutConstraint(item: closeButton, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 50)
+        let heightConstraint = NSLayoutConstraint(item: closeButton, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 50)
+        
+        view.addSubview(closeButton)
+        
+        view.addConstraint(topConstraint)
+        view.addConstraint(leadingConstraint)
+        view.addConstraint(widthConstraint)
+        view.addConstraint(heightConstraint)
+    }
+    
+    func setupActionButton() {
+        actionButton = UIButton(type: .Custom)
+        actionButton.frame = CGRectMake(0, 0, 50, 50)
+        actionButton.translatesAutoresizingMaskIntoConstraints = false
+        actionButton.setImage(UIImage(named: "ic_share"), forState: UIControlState.Normal)
+        actionButton.addTarget(self, action: #selector(actionButtonTapped), forControlEvents: .TouchUpInside)
+        
+        //Set up autolayout
+        let bottomConstraint = NSLayoutConstraint(item: view, attribute: .Bottom, relatedBy: .Equal, toItem: actionButton, attribute: .Bottom, multiplier: 1, constant: 5)
+        let leadingConstraint = NSLayoutConstraint(item: view, attribute: .Leading, relatedBy: .Equal, toItem: actionButton, attribute: .Leading, multiplier: 1, constant: -5)
+        let widthConstraint = NSLayoutConstraint(item: actionButton, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 50)
+        let heightConstraint = NSLayoutConstraint(item: actionButton, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 50)
+        
+        view.addSubview(actionButton)
+        
+        view.addConstraint(bottomConstraint)
+        view.addConstraint(leadingConstraint)
+        view.addConstraint(widthConstraint)
+        view.addConstraint(heightConstraint)
+    }
+    
+    func closeButtonTapped() {
+        dismissAnimated(true)
+    }
+    
+    func actionButtonTapped() {
+        let shouldPresentNativeSheet = delegate?.photoViewerShouldPresentBuiltInActionSheetForPhotoAtIndex(self, index: currentIndex)
+        if shouldPresentNativeSheet == true {
+            let actionSheetController = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+            actions?.forEach { actionTitle in
+                let action = UIAlertAction(title: actionTitle, style: UIAlertActionStyle.Default, handler: {
+                    _ in
+                    self.delegate?.photoViewerDidSelectActionForPhotoAtIndex(self, action: actionTitle, index: self.currentIndex)
+                })
+                actionSheetController.addAction(action)
+            }
+            
+            actionSheetController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: {     _ in
+                actionSheetController.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            
+            presentViewController(actionSheetController, animated: true, completion: nil)
         }
     }
 }
